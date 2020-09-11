dd40ee3879c21edccac01fc62a4033fc0380ddd1
Max Sierra <fr3shkid16@gmail.com>
2020-09-11 16:45:47 -0400

    Sticky questions header.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import ThankYou from "./ThankYou";
 import Form from "./Form";
 import FileUploader from "./FileUploader";
@@ -10,7 +10,6 @@ function App() {
       <Route exact path="/" component={FileUploader} />
       <Route exact path="/form/:id" component={Form} />
       <Route exact path="/thanks" component={ThankYou} />
-      <Redirect from="*" to="/" />
     </Router>
   );
 }
--- src/components/QuestionsHeader.js
+++ src/components/QuestionsHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function QuestionsHeader() {
   return (
-    <div className="flex justify-between bg-gray-200 w-10/12 md:w-6/12">
+    <div className="flex justify-between bg-gray-200 w-10/12 md:w-6/12 sticky top-0" >
       <div className="w-6/12 bg-white"></div>
       <div><span role="img" aria-label="excellent">😍</span></div>
       <div><span role="img" aria-label="good">😃</span></div>
