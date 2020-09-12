7b9c45e9cf4a169ea218a7d067b1631b7720f282
Max Sierra <fr3shkid16@gmail.com>
2020-09-11 21:41:28 -0400

    Sticky question header

--- src/components/QuestionsHeader.js
+++ src/components/QuestionsHeader.js
@@ -2,13 +2,15 @@ import React from "react";
 
 export default function QuestionsHeader() {
   return (
-    <div className="flex justify-between bg-gray-200 w-10/12 md:w-6/12 sticky top-0" >
-      <div className="w-6/12 bg-white"></div>
-      <div><span role="img" aria-label="excellent">😍</span></div>
-      <div><span role="img" aria-label="good">😃</span></div>
-      <div><span role="img" aria-label="ok">👌🏽</span></div>
-      <div><span role="img" aria-label="bad">😤</span> </div>
-      <div><span role="img" aria-label="terrible">💩</span></div>
+    <div className="flex justify-end w-10/12 md:w-6/12 sticky top-0 mb-5" >
+      <div className="flex justify-around w-1/2 bg-gray-200 py-2">
+        <div><span role="img" aria-label="excellent">😍</span></div>
+        <div><span role="img" aria-label="good">😃</span></div>
+        <div><span role="img" aria-label="ok">👌🏽</span></div>
+        <div><span role="img" aria-label="bad">😤</span> </div>
+        <div><span role="img" aria-label="terrible">💩</span></div>
+      </div>
+      
     </div>
   );
 }
